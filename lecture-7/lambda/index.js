63281dcb63a92dfcc79429d2f192b9d2951ab4c2
prime89-hash <67125778+prime89-hash@users.noreply.github.com>
2025-09-04 13:05:33 +0100

    Update index with python code

--- lecture-7/lambda/index.js
+++ lecture-7/lambda/index.js
@@ -1,12 +1,15 @@
-exports.handler = async (event) => {
-  let envValue = process.env.VIDEO_NAME
+import os
+import json
 
-  const response = {
-    statusCode: 200,
-    headers: {
-          "Content-Type": "application/json"
-    },
-    body: JSON.stringify('Message from ' + envValue),
-  };
-  return response;
-};
+def lambda_handler(event, context):
+    # Get environment variable
+    env_value = os.getenv("VIDEO_NAME", "DefaultVideo")
+
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(f"Message from {env_value}")
+    }
+    return response
